4365cec5aa537fa65a585ae4cba507d40080eef1
yd-tw <yd960528@gmail.com>
2025-03-02 11:43:10 +0800

    change blogs style

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -8,36 +8,37 @@ export default function BlogList({ limit }: { limit?: number }) {
     .slice(0, limit ?? getAllPosts().length);
 
   return (
-    <section className="bg-orange-100 dark:bg-gray-800 p-4 my-8 rounded-xl space-y-6" id="contest">
-      <h2 className="my-4 text-center text-4xl font-bold text-orange-500">部落格</h2>
-
+    <section className="p-4 my-8 rounded-xl space-y-6" id="blogs">
+      <h2 className="my-4 text-center text-4xl font-bold">部落格</h2>
       {posts.map((post) => (
-        <Card key={post.slug} className=" bg-orange-300">
-          <CardHeader>
-            <CardTitle>{post.metadata.title}</CardTitle>
-            <CardDescription>{post.metadata.publishedAt}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>{post.metadata.description}</p>
-          </CardContent>
-          <CardFooter>
-            {post.metadata.tags?.map((tag: string) => (
-              <span key={tag} className="bg-blue-100 text-blue-600 text-xs font-medium px-2 py-1 rounded-full">
-                #{tag}
-              </span>
-            ))}
-            {limit && (
-              <div className="flex justify-center my-6">
-                <Link
-                  href="/blogs"
-                  className="px-6 py-3 text-white text-lg font-semibold bg-orange-500 rounded-full shadow-md hover:bg-orange-600 transition-colors"
-                >
-                  查看更多
-                </Link>
-              </div>
-            )}
-          </CardFooter>
-        </Card>
+        <Link key={post.slug} href={`/blogs/${post.slug}`} passHref>
+          <Card className='shadow-md hover:shadow-lg transition-shadow m-4'>
+            <CardHeader>
+              <CardTitle>{post.metadata.title}</CardTitle>
+              <CardDescription>{post.metadata.publishedAt}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{post.metadata.description}</p>
+            </CardContent>
+            <CardFooter>
+              {post.metadata.tags?.map((tag: string) => (
+                <span key={tag} className="bg-blue-100 text-blue-600 text-xs font-medium px-2 py-1 rounded-full">
+                  #{tag}
+                </span>
+              ))}
+              {limit && (
+                <div className="flex justify-center my-6">
+                  <Link
+                    href="/blogs"
+                    className="px-6 py-3 text-white text-lg font-semibold bg-orange-500 rounded-full shadow-md hover:bg-orange-600 transition-colors"
+                  >
+                    查看更多
+                  </Link>
+                </div>
+              )}
+            </CardFooter>
+          </Card>
+        </Link>
       ))}
     </section>
   );
